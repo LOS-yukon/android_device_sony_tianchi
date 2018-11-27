b56805c744ee53f505ef727d1452e4d6d358c184
infus38 <infus38@gmail.com>
2018-11-27 12:00:57 -0800

    tianchi: move mixer_paths.xml under /vendor source: https://github.com/galaxyfreak/device-sony-eagle/commit/42d0233988c3502b675a0900ac805a5f2cf8ce95
    Signed-off-by: infus38 <infus38@gmail.com>

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Device etc
 PRODUCT_COPY_FILES := \
     device/sony/tianchi/rootdir/system/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
-    device/sony/tianchi/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
+    device/sony/tianchi/rootdir/system/vendor/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
     device/sony/tianchi/rootdir/system/etc/thermal-engine-8226.conf:system/etc/thermal-engine-8226.conf \
     device/sony/tianchi/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/sony/tianchi/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf

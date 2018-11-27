rmnhg/device-sony-flamingo

Replace thermanager config with thermal-engine config

device.mk
@@ -18,7 +18,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Device etc
 PRODUCT_COPY_FILES := \
     device/sony/flamingo/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/sony/flamingo/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
+    device/sony/flamingo/rootdir/system/etc/thermal-engine-8226.conf:system/etc/thermal-engine-8226.conf \
     device/sony/flamingo/rootdir/system/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
     device/sony/flamingo/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/sony/flamingo/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf

LOS-yukon/android_device_sony_tianchi

device.mk
@@ -19,7 +19,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_COPY_FILES := \
     device/sony/tianchi/rootdir/system/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
     device/sony/tianchi/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml \
-    device/sony/tianchi/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
+    device/sony/tianchi/rootdir/system/etc/thermal-engine-8226.conf:system/etc/thermal-engine-8226.conf \
     device/sony/tianchi/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
     device/sony/tianchi/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
 
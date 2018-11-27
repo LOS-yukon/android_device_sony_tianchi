a200af6d127f96b31f26b89e22b409881fa8739e
infus38 <infus38@gmail.com>
2018-11-27 11:58:47 -0800

    tianchi: rearrange tree
    Signed-off-by: infus38 <infus38@gmail.com>

--- dmesgdump/Android.mk
+++
@@ -1,6 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= dmesgdump.c
-LOCAL_MODULE := dmesgdump
-include $(BUILD_EXECUTABLE)
--- logdump/Android.mk
+++ logdump/Android.mk
@@ -4,3 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= logdump.c
 LOCAL_MODULE := logdump
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= dmesgdump.c
+LOCAL_MODULE := dmesgdump
+include $(BUILD_EXECUTABLE)
